60016ba07e7badffee908980f307c0fe9eac8531
William Thomas <wt4@icloud.com>
2023-09-10 22:55:06 -0400

    committing first draft code and file

--- reviews.py
+++ reviews.py
@@ -1,2 +1,19 @@
-# add your code here
+# import pandas
+import pandas as pd
 
+#read in data
+reviews = pd.read_csv("data/winemag-data-130k-v2.csv.zip")
+
+#create new dataframe grouped by unique country count and average reviews
+summary = reviews.groupby("country").agg({
+    "country": "count", "points": "mean"}).round(1)
+
+summary = summary.rename(columns = {"country":"count"})
+summary = summary.sort_values("count", ascending = False)
+
+#create data frame
+summary_df = pd.DataFrame(summary)
+
+#dataframe to csv
+
+summary_df.to_csv("data/reviews-per-country.csv")
